c41e540405ecaa5133f4e48053e8888e0aea56eb
edvind <joel.bergroth@gmail.com>
2016-10-17 18:26:56 +0200

    Updated DM function

--- includes/class-exhibition-shared.php
+++ includes/class-exhibition-shared.php
@@ -73,10 +73,34 @@ class Exhibition_Shared {
   	$json_feed = file_get_contents( $url, false, $args );
   	$json_feed = json_decode( $json_feed );
   	
-  	// Import each exhibition as a post
-  	foreach ($json_feed->response->docs as $exhibition):
+    $local_uuids = $this->get_meta_values( '_exhibition_id_uuid' );
   	
-  	    $this->exhibition_insert_post( $exhibition->{'artifact.uuid'} );
+  	// Search through all found exhibitions
+  	foreach ($json_feed->response->docs as $exhibition):
+  	    
+  	    foreach ($local_uuids as $local_uuid):
+  	    
+  	      // See if UUID exists in exhibition database
+  	      if ( $local_uuid->meta_value == $exhibition->{'artifact.uuid'} ):
+
+    	      $post_metadata = get_post_meta( $local_uuid->post_id );
+            
+            // If post is marked for synchronization
+            if( isset( $post_metadata["_exhibition_synchronize"] ) ):
+            
+              echo 'UUID exists and post is marked for synchronization, update post with id: '.$local_uuid->post_id.'<br/>';
+              $this->exhibition_insert_post( $exhibition->{'artifact.uuid'} );
+              
+            endif;
+            
+  	      else:
+  	      
+  	        echo 'UUID doesn\'t exist, insert following post<br/>';
+            $this->exhibition_insert_post( $exhibition->{'artifact.uuid'} );
+            
+  	      endif;
+  	    
+  	    endforeach;
   	
   	endforeach;
   }
@@ -89,6 +113,37 @@ class Exhibition_Shared {
 	 */
 	public function exhibition_insert_post( $uuid ) {
     
+    $exhibition = $this->fetch_dm_exhibition( $uuid );
+  	
+  	echo $exhibition->title;
+    echo '<br>';
+  	echo $exhibition->uuid;
+  	echo '<br>';
+  	echo $exhibition->unique_id;
+  	echo '<br>';
+  	echo $exhibition->id;
+  	echo '<br>';
+  	echo $exhibition->date_start;
+  	echo '<br>';
+  	echo $exhibition->date_end;
+  	echo '<br>';
+  	echo $exhibition->description_ingress;
+  	echo '<br>';
+  	echo $exhibition->description;
+  	echo '<br><br>';
+  	
+  }
+  
+/**
+	 * Import found exhibition to the exhibition custom post type.
+	 *
+	 * @since 		1.0.0
+	 * @var       string    $uuid    Unique ID of DigitaltMuseum post eg. CC52D261-405C-47C6-88EF-4FD7D4EF725C
+	 * @return    object/false       Returns object with exhibition information if exhibition exists on DigitaltMuseum,
+	 *                               otherwise returns as false.
+	 */
+	public function fetch_dm_exhibition( $uuid ) {
+    
     $url = 'http://api.dimu.org/artifact/uuid/'.$uuid;
     
   	$args = stream_context_create(array('http'=>
@@ -96,6 +151,10 @@ class Exhibition_Shared {
           'timeout' => 2500,
       )
   	));
+  	
+  	if ($this->get_http_response_code( $url ) != "200") {
+    	return false;
+  	}
   	
   	$json_feed = file_get_contents( $url, false, $args );
   	$json_feed = json_decode( $json_feed );
@@ -108,30 +167,20 @@ class Exhibition_Shared {
   	  
   	endforeach;
   	
-  	$exhibition_uuid = $uuid;
-  	$exhibition_date_start = $this->dm_date_to_wp( $json_feed->exhibition->timespan->fromDate );
-  	$exhibition_date_end = $this->dm_date_to_wp( $json_feed->exhibition->timespan->toDate );
-  	$exhibition_unique_id = $json_feed->unique_id;
-    $exhibition_id = $json_feed->identifier->id;
-  	$exhibition_description = nl2br ($json_feed->description);
+    $exhibition = (object) [
+      'title' => $exhibition_title,
+      'uuid' => $uuid,
+      'unique_id' => $json_feed->unique_id,
+      'id' => $json_feed->identifier->id,
+      'date_start' => $this->dm_date_to_wp( $json_feed->exhibition->timespan->fromDate ),
+      'date_end' => $this->dm_date_to_wp( $json_feed->exhibition->timespan->toDate ),
+      'description_ingress' => nl2br( $json_feed->exhibition->description ),
+      'description' => nl2br( $json_feed->description ),
+    ];
   	
-  	echo $exhibition_title;
-    echo '<br>';
-  	echo $exhibition_uuid;
-  	echo '<br>';
-  	echo $exhibition_unique_id;
-  	echo '<br>';
-  	echo $exhibition_id;
-  	echo '<br>';
-  	echo $exhibition_date_start;
-  	echo '<br>';
-  	echo $exhibition_date_end;
-  	echo '<br>';
-  	echo $exhibition_description;
-  	echo '<br><br>';
-  	
+  	return $exhibition;
   }
-  
+
   /**
 	 * Convert DM start/end dates to custom field date
 	 *
@@ -144,6 +193,37 @@ class Exhibition_Shared {
     $date_str = substr_replace($date_str, '-', 6, 0);
     $date_str = substr_replace($date_str, '-', 4, 0);
     return $date_str;
+  }
+  
+  public function get_meta_values( $key = '', $type = 'exhibition') {
+
+    global $wpdb;
+
+    if( empty( $key ) )
+        return;
+
+    $r = $wpdb->get_results( $wpdb->prepare( "
+      SELECT pm.meta_value, pm.post_id FROM {$wpdb->postmeta} pm
+      LEFT JOIN {$wpdb->posts} p ON p.ID = pm.post_id
+      WHERE pm.meta_key = '%s'
+      AND p.post_type = '%s'
+    ", $key, $type ), OBJECT_K );
+
+    return $r;
+  }
+  
+  /**
+	 * Get response code of http request
+	 *
+	 * @since 		1.0.0
+	 * @var       string    $url     URL to check
+	 * @return    string             Response code
+	 */  
+  public function get_http_response_code( $url ) {
+    
+    $headers = get_headers( $url );
+    return substr( $headers[0], 9, 3 );
+    
   }
 	
 } // class
