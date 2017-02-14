15f796d7354583139853967edcdd50958e307d6e
edvind <joel.bergroth@gmail.com>
2017-02-14 13:18:05 +0100

    Commented out unused actions

--- includes/class-exhibition.php
+++ includes/class-exhibition.php
@@ -193,8 +193,8 @@ class Exhibition {
 
 		$plugin_public = new Exhibition_Public( $this->get_plugin_name(), $this->get_version() );
 
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+		//$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+		//$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 	}
 
